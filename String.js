5103e6cf1df88e0ff2560c165f23b5e3e92580aa
Nur <snrkabatas@hotmail.com>
2020-05-27 11:05:12 +0300

    Create String.js

---
+++ String.js
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+
+    
+        System.out.println(A.length()+ B.length());
+         if ( A.compareToIgnoreCase(B) < 0 )  
+            System.out.println("No");
+        else
+            System.out.println("Yes");
+
+ 
+        System.out.println(A.substring(0, 1).toUpperCase()+ A.substring(1).toLowerCase()+" " +
+         B.substring(0, 1).toUpperCase()+ B.substring(1).toLowerCase());
+        
+    }
+}
+
+//A ve Bnin uzunluk toplamı
+//
+//ilk harfi büyük harfe çevirip boşlukla ayırma
+
+
+
